8a09db15e2ba034f353fef241100c700ae0e385b
spalmer8891 <spalmer8891@users.noreply.github.com>
2019-08-06 12:23:04 -0400

    Master Page Nav Bar
    modifications made to master page Nav bar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
         }
     });
 
-    $(".smartphones").click(function() {
-        $("#inner_container").load("views/SmartPhones.html"); //load contact us page on click
+    $(".usedcars").click(function() {
+        $("#inner_container").load("views/UsedCars.html"); //load contact us page on click
         $("#wowslider-container").hide();
     });
 
@@ -30,40 +30,96 @@ $(document).ready(function() {
         $("#wowslider-container").hide();
     });
 
-    $(".faqs").click(function() {
-        $("#inner_container").load("views/FAQs.html"); //load contact us page on click
+    $(".services").click(function() {
+        $("#inner_container").load("views/Services.html"); //load contact us page on click
         $("#wowslider-container").hide();
     });
     $(".partners").click(function() {
         $("#inner_container").load("views/partners.html"); //load partners us page on click
         $("#wowslider-container").hide();
     });
-    $('.home a').addClass('highlight'); //set home to highlighted on page load
+    $('.main').addClass('highlight'); //set home to highlighted on page load
 
-    $('#headerNav li a').click(function() {
-        $('#headerNav li a').removeClass('highlight');
-        $(this).addClass('highlight');
+    //highlight current page
+    $('.home,.usedcars,.services,.aboutus,.contactus').click(function() {
+
+        var pageIdentArr = this.className;
+        var pageIdent = pageIdentArr.split(" ");
+        if (pageIdent[0] == "home") {
+            $("#usedcars").removeClass('highlight');
+            $("#aboutus").removeClass('highlight');
+            $("#contactus").removeClass('highlight');
+            $("#services").removeClass('highlight');
+            $("#home").addClass('highlight');
+        } else if (pageIdent[0] == "services") {
+            $("#home").removeClass('highlight');
+            $("#aboutus").removeClass('highlight');
+            $("#contactus").removeClass('highlight');
+            $("#usedcars").removeClass('highlight');
+            $("#services").addClass('highlight');
+        } else if (pageIdent[0] == "usedcars") {
+            $("#home").removeClass('highlight');
+            $("#services").removeClass('highlight');
+            $("#aboutus").removeClass('highlight');
+            $("#contactus").removeClass('highlight');
+            $("#usedcars").addClass('highlight');
+        } else if (pageIdent[0] == "aboutus") {
+            $("#home").removeClass('highlight');
+            $("#contactus").removeClass('highlight');
+            $("#usedcars").removeClass('highlight');
+            $("#services").removeClass('highlight');
+            $("#aboutus").addClass('highlight');
+        } else if (pageIdent[0] == "contactus") {
+            $("#home").removeClass('highlight');
+            $("#usedcars").removeClass('highlight');
+            $("#services").removeClass('highlight');
+            $("#aboutus").removeClass('highlight');
+            $("#contactus").addClass('highlight');
+        }
     });
+    // $('#headerNav li a').click(function() {
+    //     $('#headerNav li a').removeClass('highlight');
+    //     $(this).addClass('highlight');
+    // });
 
     $(window).resize(function() { //performs functionality based on screen resize event
         var pageIdent = $("#pageIdent").val();
 
-        if (window.matchMedia("(min-width: 769px)").matches && pageIdent == "homePage") //if media query size min-width is 769px show slider
+        if (window.matchMedia("(min-width: 769px)").matches && pageIdent == "home") //if media query size min-width is 769px show slider
         {
             $("#wowslider-container").show();
+
         } else {
             $("#wowslider-container").hide();
         }
 
     });
 
-    $("#login-btn").click(function() {
+    $("#login-btn,#logout").click(function() {
         window.location = "../../views/Login.html";
     });
 
 });
 
+function mobileDropDown() {
+    var x = document.getElementById("mainNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
+function DesktopDropDown() {
+    if (window.matchMedia("(min-width: 769px)").matches) {
+        var x = document.getElementById("mainNav");
+        if (x.className === "responsive") {
+            x.className += "topnav";
+        } else {
+            x.className = "responsive";
+        }
+    }
+}
 /**
  * Get the button, and when the user clicks on it, execute navButton function
  * 
@@ -85,27 +141,15 @@ function navButton() {
 
 
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-    document.querySelectorAll('*'),
-    function(el) {
-        if (el.offsetWidth > docWidth) {
-            console.log(el);
-        }
-    }
-);
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
