a86ee75377603490703790bafeebb8b466be0942
spalmer8891 <spalmer8891@users.noreply.github.com>
2019-08-05 23:24:25 -0400

    Update FrontPage
    -modifications made to fix horizontal scroll
    -adjusted page banner

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -99,24 +99,13 @@ window.onclick = function(event) {
 }
 
 
-/* code for auto slide*/
+var docWidth = document.documentElement.offsetWidth;
 
-/*var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("cellphoneSlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}*/
+[].forEach.call(
+    document.querySelectorAll('*'),
+    function(el) {
+        if (el.offsetWidth > docWidth) {
+            console.log(el);
+        }
+    }
+);
